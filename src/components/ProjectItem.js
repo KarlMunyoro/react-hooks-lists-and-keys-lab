5a8ff9b0eae634da6133dbc944d920d9a9809f6e
Karl Munyoro Kamau <kxrlmunyoro@gmail.com>
2025-05-31 18:28:36 +0300

    Completed assignments

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,8 +5,8 @@ function ProjectItem({ name, about, technologies }) {
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      <div className="technologies" >
+        {technologies.map((x, i) => <span key={i}>{x}</span>)}
       </div>
     </div>
   );
